7fd19d692bdfc5491e102a4f8f5089c3c4d5d5d1
Sumit Akoliya <akoliyasumit@hotmail.com>
2024-11-01 02:25:38 +0530

    Added ProductImageUrl

--- Data/Migrations/20241031205258_AddedImageUrl.Designer.cs
+++ Data/Migrations/20241031205258_AddedImageUrl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpicyMealRestaurant.Data;
 
@@ -11,9 +12,11 @@ using SpicyMealRestaurant.Data;
 namespace SpicyMealRestaurant.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241031205258_AddedImageUrl")]
+    partial class AddedImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
